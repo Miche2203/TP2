e1a29f25b4320d42fa0040c116a6d1def617d17e
gloria Michelena <gloria.michelena@gmail.com>
2025-03-24 13:48:19 -0300

    2 commit

--- Clase-1/index.js
+++ Clase-1/index.js
@@ -1,2 +1,33 @@
+console.log("")
 console.log("Hola mundo!")
 console.log("Bienvenidos")
+
+function saludo() {
+    const saludo = "Bienvenidos!!!!!!!"
+    console.log(saludo)
+ }
+ saludo()
+
+ const saludar = function darSaludo() {
+    const saludo = "Holisss"
+    console.log(saludo)
+    return saludo
+ }
+
+saludar() 
+console.log(saludar())
+
+const val1 = 5
+const val2 = 2
+const data = (a, b) =>{
+console.log("Suma parametros: ", a + b)
+}
+data (val1, val2)
+
+let xyz = "variable xyz"
+function imprimirValor() {
+    let xyz = "Variable xyz dentro de la Fx"
+}
+console.log(xyz)
+ console.log("")
+ 
