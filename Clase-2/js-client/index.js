41176f496f61155fc03ef43772624ea797d0875c
gloria Michelena <gloria.michelena@gmail.com>
2025-04-03 09:30:26 -0300

    Clase 2

---
+++ Clase-2/js-client/index.js
@@ -0,0 +1,17 @@
+// const saludo = "Hola"
+
+// setInterval(() => {
+//      // logica aca
+//     console.log("Set interval...")
+//  }, 2000) // milisegundos
+
+// setTimeout(() => {
+//     console.log("Set timeout...")
+// }, 2000);
+
+alert("Hola mundo!");
+console.log("TEST")
+
+document.getElementById("btn").addEventListener("click", function name(){
+document.getElementById("text").textContent = "Boton clickeado!"
+})
